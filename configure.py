ce0b6a2ff21e0d68544683ec9c57738ec84bb83c
Galic-parmesan-wings <53062441+Galic-parmesan-wings@users.noreply.github.com>
2020-04-22 12:13:09 -0700

    commit5

--- configure.py
+++ configure.py
@@ -6,17 +6,17 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'wocaonima110'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    ELASTICSEARCH_URL = 'http://localhost:9200'
-    LANGUAGES = ['en', 'es']
-    POSTS_PER_PAGE = 5
-    ADMINS = ['Fenglinjoyyy@gmail.com']
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = 1
-    MAIL_USERNAME = 'Fenglinjoyyy@gmail.com'
-    MAIL_PASSWORD = 'ZFl5948405'
-    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT') or 1
+    SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+    ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
+    LANGUAGES = os.environ.get('LANGUAGES')
+    POSTS_PER_PAGE = os.environ.get('POSTS_PER_PAGE')
+    ADMINS = os.environ.get('ADMINS')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
