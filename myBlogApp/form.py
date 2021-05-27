eb099516ac45f39513c654eb9460a54677422585
Galic-parmesan-wings <Fenglinjoyyy@gmail.com>
2021-05-26 20:37:47 -0700

    update password reset page

--- myBlogApp/form.py
+++ myBlogApp/form.py
@@ -89,7 +89,7 @@ class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField(
         'Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Request Password Reset')
+    submit = SubmitField('Reset Password')
 
 
 class MessageForm(FlaskForm):
